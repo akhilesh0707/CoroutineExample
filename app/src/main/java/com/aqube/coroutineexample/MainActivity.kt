dcd242d825ca89302e645a996fedc3ce0692e553
Akhilesh <akhilesh0707@gmail.com>
2020-08-01 21:46:58 +0530

    Coroutine: download image and convert into black and white

--- app/src/main/java/com/aqube/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/aqube/coroutineexample/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.aqube.coroutineexample
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
+    private val IMAGE_URL =
+        "https://image.freepik.com/free-vector/cute-dog-with-bone_23-2147515747.jpg"
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO) { getOriginalBitmap() }
+            val originalBitmap = originalDeferred.await()
+            loadImage(originalBitmap)
+            val originalDeferredBnW = coroutineScope.async { filterBitmap(originalBitmap) }
+            loadImageBnW(originalDeferredBnW.await())
+        }
+    }
+
+    private fun getOriginalBitmap() =
+        URL(IMAGE_URL).openStream().use { BitmapFactory.decodeStream(it) }
+
+    private fun filterBitmap(bitmap: Bitmap) = Filter.apply(bitmap)
+
+    private fun loadImage(bitmap: Bitmap) {
+        imageView.apply {
+            setImageBitmap(bitmap)
+            visibility = View.VISIBLE
+        }
+    }
+
+    private fun loadImageBnW(bitmap: Bitmap) {
+        progressBar.visibility = View.GONE
+        imageViewBnW.apply {
+            setImageBitmap(bitmap)
+            visibility = View.VISIBLE
+        }
     }
 }
