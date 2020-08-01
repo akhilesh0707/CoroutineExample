3d8e077f2578a6009c4e82ad9b2c1ba761a2838e
Akhilesh <akhilesh0707@gmail.com>
2020-08-02 01:57:24 +0530

    Coroutine: Change navigation setup

--- app/src/main/java/com/aqube/coroutineexample/ui/MainActivity.kt
+++ app/src/main/java/com/aqube/coroutineexample/ui/MainActivity.kt
@@ -3,13 +3,33 @@ package com.aqube.coroutineexample.ui
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.aqube.coroutineexample.R
-
+import androidx.navigation.ui.setupActionBarWithNavController
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        setUpNavigation()
+    }
+
+    /**
+     * Setup the action bar so that the navigation controller controls navigation
+     */
+    private fun setUpNavigation() {
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navGraph = navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
